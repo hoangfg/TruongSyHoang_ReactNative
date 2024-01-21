9bff9d50533a03f19e8b9d28bbafcf9da151f69d
hoangfg <121874187+hoangfg@users.noreply.github.com>
2024-01-21 20:17:49 +0700

    build apk

--- src/api/ApiBase.js
+++ src/api/ApiBase.js
@@ -3,6 +3,7 @@ import axios from "axios"
 
 const ApiBase = axios.create({
     baseURL: "http://192.168.38.236:8080/api",
+    //192.168.38.236
     responseType: 'json',
     withCredentials: true
 })
